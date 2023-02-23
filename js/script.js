355d57aff17d13e841bd7faf8b91b7a001a799b0
christianarca27 <christian.arca.mail@gmail.com>
2023-02-23 13:29:33 +0100

    feat: migliorata la visualizzazione del risultato

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
+const prezzoPerKm = 0.21;
+
+// Leggo e salvo il riferimento al bottone di generazione
 let buttonEl = document.getElementById("btn-genera");
 console.log(buttonEl);
 
+// Evento nel caso venga cliccato il bottone di generazione del biglietto
 buttonEl.addEventListener("click", function() {
+    //Leggo e salvo il valore dell'input "Nome Cognome"
     let nomePasseggeroEl = document.getElementById("nome-passeggero");
     let nomePasseggero = nomePasseggeroEl.value;
     console.log(nomePasseggero);
 
+    //Leggo e salvo il valore dell'input "Km da percorrere"
     let kmPercorsiEl = document.getElementById("km-da-percorrere");
     let kmPercorsi = kmPercorsiEl.value;
     console.log(kmPercorsi);
     
+    //Leggo e salvo il valore dell'input "Fascia d'età"
     let fasciaEtaEl = document.getElementById("fascia-eta");
     let fasciaEta = fasciaEtaEl.value;
     console.log(fasciaEta);
 
-    let prezzoBiglietto = kmPercorsi * 0.21;
+    // Calcolo il prezzo del biglietto a meno di scontistiche
+    let prezzoBiglietto = kmPercorsi * prezzoPerKm;
 
+    // Calcolo la scontistica se mi trovo in una delle fasce d'età che la prevede
     if(fasciaEta == "minorenne") {
         prezzoBiglietto *= 0.8;
     }
@@ -23,5 +32,6 @@ buttonEl.addEventListener("click", function() {
         prezzoBiglietto *= 0.6;
     }
 
-    document.writeln("Il prezzo del biglietto di " + nomePasseggero + " è di " + prezzoBiglietto + "€");
+    // TEST: Stampo in pagina il risultato finale
+    document.getElementById("biglietto").innerText = "Il prezzo del biglietto di " + nomePasseggero + " è di " + prezzoBiglietto + "€";
 })
